93631eeb6c83823b6db46b93eef80d33e55640cb
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-09-23 23:52:44 +0800

    withdraw api testing suite

---
+++ src/__tests__/withdraw.ts
@@ -0,0 +1,59 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+
+import server from "../app";
+import Withdraw, { WithdrawModel } from "../models/Withdraw";
+import Balance, { BalanceModel } from "../models/Balance";
+
+const should = chai.should();
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+context("dummy-banking withdraw API", () => {
+  describe("withdraw api", () => {
+    // Before each test we empty the database
+    beforeEach((done) => {
+      Withdraw.remove({}, (error: Error) => {
+        done();
+      });
+    });
+
+    it ("GET all withdraws", (done) => {
+      chai
+        .request(server)
+        .get("/api/1.0/withdraws")
+        .end((error, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          expect(res.body.withdraws).to.be.a("array");
+          done();
+        });
+    });
+
+    it("GET withdraw via ID", (done) => {
+      const newBalance = new Balance({
+        amount: 1000
+      });
+      // saving a dummy balance
+      newBalance.save((error: Error, balance: BalanceModel) => {
+        const newWithdraw = new Withdraw({
+          amount: 5000,
+          balanceId: balance._id
+        });
+        newWithdraw.save((error: Error, withdraw: WithdrawModel) => {
+          chai
+            .request(server)
+            .get(`/api/1.0/withdraws/${withdraw._id}`)
+            .end((error: Error, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a("object");
+              res.body.withdraw.should.be.a("object");
+              expect(res.body.withdraw.amount).to.equal(5000);
+              done();
+            });
+        });
+      });
+    });
+  });
+});
